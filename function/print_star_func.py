bb0aecd5f5db1ab222ba8fda2b950026ee2818a0
PalGuPyePyeGi <luvkbstar@gmail.com>
2021-04-15 17:47:30 +0900

    function

---
+++ function/print_star_func.py
@@ -0,0 +1,3 @@
+def print_star(a,c,b=5):
+    print('************', a+b+c)
+print_star(c= 7, a= 10)
---
+++ function/sum_func_global2.py
@@ -0,0 +1,11 @@
+def print_sum():
+    global a
+    global b
+    a = 100
+    b = 200
+    result = a + b
+    print('print_sum() 내부 :', a, '과', b, '의 합은', result, '입니다.')
+# a = 10
+# b = 20
+print(a+b)
+print_sum()
